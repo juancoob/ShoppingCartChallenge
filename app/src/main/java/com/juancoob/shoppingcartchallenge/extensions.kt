164895c75ebf13626bd2c75c7ed7bb1894a322ca
juancoob <ja.cobos.obrero@gmail.com>
2022-06-15 11:33:47 +0200

    feat: Create tryCall to manage API results trycall method is intended to create an Either to store an error or the data retrieved. Throwable exceptions are mapped to ErrorRetrieved types.

---
+++ app/src/main/java/com/juancoob/shoppingcartchallenge/extensions.kt
@@ -0,0 +1,20 @@
+package com.juancoob.shoppingcartchallenge
+
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import java.io.IOException
+import com.juancoob.domain.ErrorRetrieved
+import retrofit2.HttpException
+
+suspend fun <T> tryCall(action: suspend () -> T): Either<ErrorRetrieved, T> = try {
+    action().right()
+} catch (e: Exception) {
+    e.toErrorRetrieved().left()
+}
+
+fun Throwable.toErrorRetrieved(): ErrorRetrieved = when(this) {
+    is IOException -> ErrorRetrieved.Connectivity
+    is HttpException -> ErrorRetrieved.Server(code())
+    else -> ErrorRetrieved.Unknown(message ?: "")
+}
