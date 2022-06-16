5b956e532756f3f4a29d351a1dc09624f06f8965
juancoob <ja.cobos.obrero@gmail.com>
2022-06-16 22:44:42 +0200

    feat: Add their use case tests

--- usecases/src/test/kotlin/com/juancoob/usecases/GetAvailableDormByIdUseCaseTest.kt
+++ usecases/src/test/kotlin/com/juancoob/usecases/GetAvailableDormByIdUseCaseTest.kt
@@ -3,14 +3,12 @@ package com.juancoob.usecases
 import com.juancoob.data.DormRepository
 import com.juancoob.testshared.mockedDorm
 import io.mockk.MockKAnnotations
-import io.mockk.coVerify
 import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class GetAvailableDormByIdUseCaseTest {
 
     @RelaxedMockK
@@ -25,9 +23,8 @@ class GetAvailableDormByIdUseCaseTest {
     }
 
     @Test
-    fun `When the user clicks on a dorm in the main list, the app retrieves its details`() =
-        runTest {
-            getAvailableDormByIdUseCase.invoke(mockedDorm.id)
-            coVerify { repository.getAvailableDormById(mockedDorm.id) }
-        }
+    fun `When the user clicks on a dorm in the main list, the app retrieves its details`() {
+        getAvailableDormByIdUseCase.invoke(mockedDorm.id)
+        verify { repository.getAvailableDormById(mockedDorm.id) }
+    }
 }
---
+++ usecases/src/test/kotlin/com/juancoob/usecases/GetStoredDormsUseCaseTest.kt
@@ -0,0 +1,31 @@
+package com.juancoob.usecases
+
+import com.juancoob.data.DormRepository
+import io.mockk.MockKAnnotations
+import io.mockk.coVerify
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class GetStoredDormsUseCaseTest {
+
+    @RelaxedMockK
+    lateinit var repository: DormRepository
+
+    lateinit var getStoredDormsUseCase: GetStoredDormsUseCase
+
+    @Before
+    fun startUp() {
+        MockKAnnotations.init(this)
+        getStoredDormsUseCase = GetStoredDormsUseCase(repository)
+    }
+
+    @Test
+    fun `When the user opens the app, it checks the dorms already stored`() = runTest {
+        getStoredDormsUseCase.invoke()
+        coVerify { repository.getStoredDorms() }
+    }
+}
