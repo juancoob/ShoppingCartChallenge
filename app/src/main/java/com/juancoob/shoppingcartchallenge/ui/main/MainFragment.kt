d0bae6c3f8ce0adfab3c07d7fc05749654a21299
juancoob <ja.cobos.obrero@gmail.com>
2022-06-16 17:29:50 +0200

    feat: Add MainFragment

---
+++ app/src/main/java/com/juancoob/shoppingcartchallenge/ui/main/MainFragment.kt
@@ -0,0 +1,66 @@
+package com.juancoob.shoppingcartchallenge.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.juancoob.domain.Dorm
+import com.juancoob.shoppingcartchallenge.R
+import com.juancoob.shoppingcartchallenge.databinding.FragmentMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var adapter: DormAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initList()
+        collectStates()
+    }
+
+    private fun initList() {
+        adapter = DormAdapter(::goToDetailFragment)
+        binding.mainList.adapter = adapter
+    }
+
+    private fun collectStates() {
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainViewModel.state.collect {
+                    binding.progressBar.isVisible = it.loading
+                    adapter.submitList(it.dorms)
+                }
+            }
+        }
+    }
+
+    private fun goToDetailFragment(dorm: Dorm) {
+        //todo
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
