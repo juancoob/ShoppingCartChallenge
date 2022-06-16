bfb6e82a099a0a4d1573f3f726c48be21a890157
juancoob <ja.cobos.obrero@gmail.com>
2022-06-16 12:45:12 +0200

    feat: Create the LocalDormDataSourceImpl

---
+++ app/src/main/java/com/juancoob/shoppingcartchallenge/data/database/LocalDormDataSourceImpl.kt
@@ -0,0 +1,73 @@
+package com.juancoob.shoppingcartchallenge.data.database
+
+import com.juancoob.data.datasource.LocalDormDataSource
+import com.juancoob.domain.Bed
+import com.juancoob.domain.Cart
+import com.juancoob.domain.Dorm
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import com.juancoob.shoppingcartchallenge.data.database.Bed as DbBed
+import com.juancoob.shoppingcartchallenge.data.database.Cart as DbCart
+import com.juancoob.shoppingcartchallenge.data.database.Dorm as DbDorm
+
+class LocalDormDataSourceImpl @Inject constructor(
+    private val dormDao: DormDao
+) : LocalDormDataSource {
+
+    override fun getAvailableDorms(): Flow<List<Dorm>> =
+        dormDao.getAvailableDorms().map { it.toDomainDormModel() }
+
+    override suspend fun getAvailableDormById(id: Int): Dorm =
+        dormDao.getAvailableDormById(id).toDomainDormModel()
+
+    override suspend fun insertDorms(dorms: List<Dorm>) =
+        dormDao.insertDorms(dorms.fromDomainDormModel())
+
+    override suspend fun updateDorm(dorm: Dorm) =
+        dormDao.updateDorm(dorm.fromDomainDormModel())
+
+    override suspend fun storeAvailableBedForCheckout(bed: Bed) =
+        dormDao.storeAvailableBedForCheckout(bed.fromDomainBedModel())
+
+    override suspend fun deleteAStoredBedForCheckout(bed: Bed) =
+        dormDao.deleteAStoredBedForCheckout(bed.fromDomainBedModel())
+
+    override suspend fun getCart(): List<Cart> =
+        dormDao.getCart().toDomainCartModel()
+}
+
+fun List<DbDorm>.toDomainDormModel(): List<Dorm> = map { it.toDomainDormModel() }
+
+fun DbDorm.toDomainDormModel(): Dorm = Dorm(
+    id = id,
+    type = type,
+    maxBeds = maxBeds,
+    bedsAvailable = bedsAvailable,
+    pricePerBed = pricePerBed
+)
+
+fun List<Dorm>.fromDomainDormModel(): List<DbDorm> = map { it.fromDomainDormModel() }
+
+fun Dorm.fromDomainDormModel(): DbDorm = DbDorm(
+    id = id,
+    type = type,
+    maxBeds = maxBeds,
+    bedsAvailable = bedsAvailable,
+    pricePerBed = pricePerBed
+)
+
+fun Bed.fromDomainBedModel(): DbBed = DbBed(
+    dormId = dormId,
+    pricePerBed = pricePerBed
+)
+
+fun List<DbCart>.toDomainCartModel(): List<Cart> = map { it.toDomainCartModel() }
+
+fun DbCart.toDomainCartModel(): Cart = Cart(
+    dormId = dormId,
+    bedsForCheckout = bedsForCheckout,
+    type = type,
+    pricePerBed = pricePerBed,
+    bedsAvailable = bedsAvailable
+)
