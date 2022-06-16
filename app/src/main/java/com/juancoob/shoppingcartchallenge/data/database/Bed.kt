dee7969c1a6e7b5b850e08e389e124c5d5f53fa0
juancoob <ja.cobos.obrero@gmail.com>
2022-06-16 22:40:42 +0200

    feat: Update DB models to fix the id generation

--- app/src/main/java/com/juancoob/shoppingcartchallenge/data/database/Bed.kt
+++ app/src/main/java/com/juancoob/shoppingcartchallenge/data/database/Bed.kt
@@ -5,7 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Bed(
-    @PrimaryKey val dormId: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int? = null,
+    val dormId: Int,
     val pricePerBed: Double,
     val currency: String
 )
--- app/src/main/java/com/juancoob/shoppingcartchallenge/data/database/Symbol.kt
+++ app/src/main/java/com/juancoob/shoppingcartchallenge/data/database/Symbol.kt
@@ -5,6 +5,6 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Symbol(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int? = null,
     val symbol: String
 )
