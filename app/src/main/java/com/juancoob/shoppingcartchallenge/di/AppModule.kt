5abdd77fd93c86492e1a3c4e5274b510e56b0e04
juancoob <ja.cobos.obrero@gmail.com>
2022-06-16 12:47:30 +0200

    feat: Update AppModule Updates for the separation of concerns and inject more modules

--- app/src/main/java/com/juancoob/shoppingcartchallenge/di/AppModule.kt
+++ app/src/main/java/com/juancoob/shoppingcartchallenge/di/AppModule.kt
@@ -1,8 +1,17 @@
 package com.juancoob.shoppingcartchallenge.di
 
+import android.app.Application
+import androidx.room.Room
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.juancoob.data.datasource.LocalCurrencyDataSource
+import com.juancoob.data.datasource.LocalDormDataSource
 import com.juancoob.data.datasource.RemoteDataSource
 import com.juancoob.shoppingcartchallenge.BuildConfig
+import com.juancoob.shoppingcartchallenge.data.database.ChallengeDatabase
+import com.juancoob.shoppingcartchallenge.data.database.DormDao
+import com.juancoob.shoppingcartchallenge.data.database.LocalCurrencyDataSourceImpl
+import com.juancoob.shoppingcartchallenge.data.database.LocalDormDataSourceImpl
+import com.juancoob.shoppingcartchallenge.data.database.SymbolDao
 import com.juancoob.shoppingcartchallenge.data.server.RemoteDataSourceImpl
 import com.juancoob.shoppingcartchallenge.data.server.RemoteService
 import com.juancoob.shoppingcartchallenge.di.qualifiers.ApiKey
@@ -21,21 +30,13 @@ import retrofit2.Retrofit
 import retrofit2.create
 import javax.inject.Singleton
 
+
+// Provides
+
 @Module
 @InstallIn(SingletonComponent::class)
 @OptIn(ExperimentalSerializationApi::class)
-object ProvidesAppModule {
-
-    @Provides
-    @Singleton
-    @ApiKey
-    fun provideApiKey(): String = BuildConfig.API_LAYER_KEY
-
-    @Provides
-    @Singleton
-    @ApiUrl
-    fun provideApiUrl(): String = "https://api.apilayer.com/exchangerates_data"
-
+object AppModuleProviderForRetrofit {
     @Provides
     @Singleton
     fun provideOkHttpClient(): OkHttpClient = HttpLoggingInterceptor().run {
@@ -58,9 +59,77 @@ object ProvidesAppModule {
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class BindsAppModule {
+object AppModuleProviderForApiKey {
+    @Provides
+    @Singleton
+    @ApiKey
+    fun provideApiKey(): String = BuildConfig.API_LAYER_KEY
+}
 
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModuleProviderForEndPoint {
+    @Provides
+    @Singleton
+    @ApiUrl
+    fun provideApiUrl(): String = "https://api.apilayer.com/exchangerates_data"
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModuleProviderForRoom {
+    @Provides
+    @Singleton
+    fun provideRoomDataBase(app: Application): ChallengeDatabase =
+        Room.databaseBuilder(
+            app,
+            ChallengeDatabase::class.java,
+            "challenge-database"
+        ).build()
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModuleProviderForSymbolDao {
+    @Provides
+    @Singleton
+    fun provideSymbolDao(challengeDatabase: ChallengeDatabase): SymbolDao =
+        challengeDatabase.symbolDao()
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModuleProviderForDormDao {
+    @Provides
+    @Singleton
+    fun provideSymbolDao(challengeDatabase: ChallengeDatabase): DormDao =
+        challengeDatabase.dormDao()
+}
+
+
+// Binds
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AppModuleBinderForRemoteDataSource {
     @Binds
     abstract fun bindRemoteDataSource(remoteDataSourceImpl: RemoteDataSourceImpl): RemoteDataSource
+}
 
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AppModuleBinderForLocalCurrencyDataSource {
+    @Binds
+    abstract fun bindLocalCurrencyDataSource(
+        localCurrencyDataSourceImpl: LocalCurrencyDataSourceImpl
+    ): LocalCurrencyDataSource
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AppModuleBinderForLocalDormDataSource {
+    @Binds
+    abstract fun bindLocalDormDataSource(
+        localDormDataSourceImpl: LocalDormDataSourceImpl
+    ): LocalDormDataSource
 }
