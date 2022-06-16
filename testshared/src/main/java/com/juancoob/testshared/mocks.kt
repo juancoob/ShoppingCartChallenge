83c4d7286420ae72624e32eb2a43a5258c7b0cf5
juancoob <ja.cobos.obrero@gmail.com>
2022-06-16 12:51:12 +0200

    refactor: Update mocks

--- testshared/src/main/java/com/juancoob/testshared/mocks.kt
+++ testshared/src/main/java/com/juancoob/testshared/mocks.kt
@@ -1,10 +1,32 @@
 package com.juancoob.testshared
 
+import com.juancoob.domain.Bed
+import com.juancoob.domain.Cart
 import com.juancoob.domain.Dorm
+
+const val FROM = "EUR"
+
+const val TO = "GBP"
+
+val mockedSymbols = listOf("EUR", "GBP")
 
 val mockedDorm = Dorm(
     id = 1,
+    type = "6-bed dorm",
     maxBeds = 6,
     bedsAvailable = 6,
-    pricePerBed = 17.56F
+    pricePerBed = 17.56
+)
+
+val mockedBed = Bed(
+    dormId = 1,
+    pricePerBed = 17.56
+)
+
+val mockedCart = Cart(
+    dormId = 1,
+    bedsForCheckout = 1,
+    type = "6-bed dorm",
+    pricePerBed = 17.56,
+    bedsAvailable = 5
 )
