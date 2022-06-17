5a0cda49a4ae7ff352f39e16072f35bb1ca29a39
juancoob <ja.cobos.obrero@gmail.com>
2022-06-17 23:44:59 +0200

    refactor: Update LocalCurrencyDataSource To sore Symbols as Strings

--- data/src/main/java/com/juancoob/data/datasource/LocalCurrencyDataSource.kt
+++ data/src/main/java/com/juancoob/data/datasource/LocalCurrencyDataSource.kt
@@ -1,11 +1,12 @@
 package com.juancoob.data.datasource
 
 import com.juancoob.domain.ErrorRetrieved
+import com.juancoob.domain.Symbol
 import kotlinx.coroutines.flow.Flow
 
 interface LocalCurrencyDataSource {
     fun getSymbols(): Flow<List<String>>
     suspend fun isSymbolListEmpty(): Boolean
-    suspend fun insertSymbols(symbols: List<String>): ErrorRetrieved?
+    suspend fun insertSymbols(symbolList: List<String>): ErrorRetrieved?
     suspend fun deleteAllSymbols()
 }
