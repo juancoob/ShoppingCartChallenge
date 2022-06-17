5befc214637b2f7f317c10ae7b3013c363aed9f5
juancoob <ja.cobos.obrero@gmail.com>
2022-06-17 23:54:47 +0200

    feat: Add navController to bottom navigation

--- app/src/main/java/com/juancoob/shoppingcartchallenge/ui/NavHostActivity.kt
+++ app/src/main/java/com/juancoob/shoppingcartchallenge/ui/NavHostActivity.kt
@@ -1,14 +1,21 @@
 package com.juancoob.shoppingcartchallenge.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.juancoob.shoppingcartchallenge.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class NavHostActivity : AppCompatActivity() {
+class NavHostActivity : AppCompatActivity(R.layout.activity_nav_host) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nav_host)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
