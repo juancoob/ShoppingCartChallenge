803ec722b8e89f8018c6fe49f2e71fbb68308c0f
juancoob <ja.cobos.obrero@gmail.com>
2022-06-17 23:44:08 +0200

    refactor: Update RemoteDataSource to get Symbols To get Symbols as server model

--- data/src/main/java/com/juancoob/data/datasource/RemoteDataSource.kt
+++ data/src/main/java/com/juancoob/data/datasource/RemoteDataSource.kt
@@ -2,8 +2,9 @@ package com.juancoob.data.datasource
 
 import arrow.core.Either
 import com.juancoob.domain.ErrorRetrieved
+import com.juancoob.domain.Symbol
 
 interface RemoteDataSource {
-    suspend fun getSymbols(): Either<ErrorRetrieved, List<String>>
+    suspend fun getSymbols(): Either<ErrorRetrieved, Symbol>
     suspend fun getConversion(from: String, to: String, amount: Double): Either<ErrorRetrieved, Double>
 }
