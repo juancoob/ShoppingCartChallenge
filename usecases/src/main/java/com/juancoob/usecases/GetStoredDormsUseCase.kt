a99c3301c976c54ecd2a42994ae4f02c53da6931
juancoob <ja.cobos.obrero@gmail.com>
2022-06-19 13:56:20 +0200

    refactor: Update use case to return dorms Instead of a dorm count

--- usecases/src/main/java/com/juancoob/usecases/GetStoredDormsUseCase.kt
+++ usecases/src/main/java/com/juancoob/usecases/GetStoredDormsUseCase.kt
@@ -1,9 +1,10 @@
 package com.juancoob.usecases
 
 import com.juancoob.data.DormRepository
+import com.juancoob.domain.Dorm
 import javax.inject.Inject
 
 class GetStoredDormsUseCase @Inject constructor(private val repository: DormRepository) {
-    suspend operator fun invoke(): Int =
+    suspend operator fun invoke(): List<Dorm> =
         repository.getStoredDorms()
 }
