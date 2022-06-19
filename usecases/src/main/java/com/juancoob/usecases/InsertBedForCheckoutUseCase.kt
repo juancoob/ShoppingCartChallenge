e13617fa6ce5fc53dac6cc1d6fc1ead3f0036584
juancoob <ja.cobos.obrero@gmail.com>
2022-06-19 13:52:56 +0200

    refactor: Update use case name

--- usecases/src/main/java/com/juancoob/usecases/InsertBedForCheckoutUseCase.kt
+++ usecases/src/main/java/com/juancoob/usecases/InsertBedForCheckoutUseCase.kt
@@ -4,7 +4,7 @@ import com.juancoob.data.DormRepository
 import com.juancoob.domain.Bed
 import javax.inject.Inject
 
-class StoreAvailableBedForCheckoutUseCase @Inject constructor(private val repository: DormRepository) {
+class InsertBedForCheckoutUseCase @Inject constructor(private val repository: DormRepository) {
     suspend operator fun invoke(bed: Bed) =
-        repository.storeAvailableBedForCheckout(bed)
+        repository.insertBedForCheckout(bed)
 }
